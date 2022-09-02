3c16f7c91eceefe0f581ac081c3755dbb9a2dc81
evkakiy <evgeniy.gladkov33@gmail.com>
2022-09-02 17:24:59 +0900

    test bootstrapp col

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layout.index')
 
 @section('content')
-<h1>Hello layout index</h1>
+<div class="container text-center">
+    <h2>Об гостинице - Якутия, г. Новосибирск</h2>
+    <div class="row justify-content-around">
+        <div class="col-4 ">
+            <h2>Информация 1</h2>
+        </div>
+        <div class="col-4">
+            <h2>Информация 2</h2>
+        </div>
+    </div>
+</div>
 @endsection
